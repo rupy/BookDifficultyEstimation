bf1ecd68ae0deb4294d7992445120b46c95f1a7b
Rupy <rupyapps@gmail.com>
2014-06-26 13:13:34 +0900

    bookのscaffoldを追加

---
+++ app/views/books/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @book, :id, :title, :asin, :node_id, :browsenode, :author, :manufacture, :url, :amount, :contents, :pre_processed_contents, :created_at, :updated_at
---
+++ db/migrate/20140626041220_create_books.rb
@@ -0,0 +1,18 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.text :title
+      t.text :asin
+      t.integer :node_id
+      t.text :browsenode
+      t.text :author
+      t.text :manufacture
+      t.text :url
+      t.integer :amount
+      t.text :contents
+      t.text :pre_processed_contents
+
+      t.timestamps
+    end
+  end
+end
